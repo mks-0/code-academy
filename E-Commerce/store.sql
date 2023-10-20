be81116905bbd6f63c94049194bc87a55a7caa73
Milan Karahiaur <milankarahiaur@gmail.com>
2023-10-20 20:38:55 +0200

    Update

--- E-Commerce/store.sql
+++ E-Commerce/store.sql
@@ -31,54 +31,46 @@ CREATE TABLE IF NOT EXISTS product (
 "sub_category" varchar(128)
 );
 
-CREATE TABLE IF NOT EXISTS shipment (
-"id" varchar(64) PRIMARY KEY,
-"ship_mode" varchar(64),
-"address_id" integer,
-"ship_date" date,
-FOREIGN KEY ("address_id") REFERENCES "address" ("id")
-);
-
 CREATE TABLE  IF NOT EXISTS orders (
 "id" varchar(32) PRIMARY KEY,
 "order_date" date,
 "customer_id" varchar(16),
-"shipment_id" varchar(64),
-FOREIGN KEY ("shipment_id") REFERENCES "shipment" ("id"),
 FOREIGN KEY ("customer_id") REFERENCES "customer" ("id")
 );
 
+CREATE TABLE IF NOT EXISTS shipment (
+"id" varchar(64) PRIMARY KEY,
+"order_id" varchar(32),
+"ship_mode" varchar(64),
+"address_id" integer,
+"ship_date" date,
+FOREIGN KEY ("order_id") REFERENCES "orders" ("id"),
+FOREIGN KEY ("address_id") REFERENCES "address" ("id")
+);
 
 CREATE TABLE IF NOT EXISTS orderitems (
 "product_id" varchar(16),
 "order_id" varchar(32),
 "discount" float,
 "quantity" integer,
+"shipment_id" varchar(64),
 PRIMARY KEY ("product_id", "order_id"),
 FOREIGN KEY ("product_id") REFERENCES "product" ("id"),
-FOREIGN KEY ("order_id") REFERENCES "orders" ("id")
+FOREIGN KEY ("order_id") REFERENCES "orders" ("id"),
+FOREIGN KEY ("shipment_id") REFERENCES "shipment" ("id")
 );
 
  -- 1. What is the category generating the maximum sales revenue?
         -- What about the profit in this category?
         -- Are they making a loss in any categories?
 
-SELECT p.category, SUM(p.sell_price *(1 - oi.discount) * oi.quantity)::numeric(30,2) as sales
-FROM Product p
-JOIN OrderItems oi ON p.id = oi.product_id
-JOIN Orders o ON o.id = oi.order_id
-GROUP BY p.category
-ORDER BY sales DESC
-LIMIT 1;
-
-SELECT p.category, 
+SELECT p.category, SUM(p.sell_price *(1 - oi.discount) * oi.quantity)::numeric(30,2) as sales,
 SUM(p.sell_price *(1 - oi.discount) * oi.quantity - oi.quantity * p.cost)::numeric(30,2) as profit
 FROM Product p
 JOIN OrderItems oi ON p.id = oi.product_id
 JOIN Orders o ON o.id = oi.order_id
 GROUP BY p.category
 ORDER BY sales DESC
-LIMIT 1;
 
 SELECT *
 FROM(
@@ -99,7 +91,7 @@ SUM(p.sell_price *(1 - oi.discount) * oi.quantity)::numeric(30,2) as sales
 FROM Product p
 JOIN OrderItems oi ON p.id = oi.product_id
 JOIN Orders o ON o.id = oi.order_id
-JOIN Shipment s ON s.id = o.shipment_id
+JOIN Shipment s ON s.order_id = o.id
 JOIN Address a ON a.id = s.address_id
 GROUP BY a.state
 ORDER BY sales;
@@ -118,38 +110,38 @@ LIMIT 5;
 
  -- 3. What are the 3 products in each product segment with the highest sales?
         -- Are they the 3 most profitable products as well?
-
 SELECT *
 FROM(
-SELECT DISTINCT st.id, st.name, st.sales, st.profit, c.segment,
-DENSE_RANK () OVER (PARTITION BY segment ORDER BY sales DESC) AS s_rank, 
-RANK () OVER (PARTITION BY segment ORDER BY profit DESC) AS p_rank
+SELECT *, 
+RANK () OVER (PARTITION BY pq.segment ORDER BY pq.sales DESC) AS s_rank,
+RANK () OVER (PARTITION BY pq.segment ORDER BY pq.profit DESC) AS p_rank
 FROM(
-SELECT p.id, p.name,
+SELECT p.id, p.name, c.segment,
 SUM(p.sell_price *(1 - oi.discount) * oi.quantity)::numeric(30,2) as sales,
 SUM(p.sell_price *(1 - oi.discount) * oi.quantity - oi.quantity * p.cost)::numeric(30,2) as profit
 FROM Product p
 JOIN OrderItems oi ON p.id = oi.product_id
-GROUP BY p.id, p.name) as st
-JOIN OrderItems oi ON st.id = oi.product_id
 JOIN Orders o ON o.id = oi.order_id
-JOIN Customer c ON c.id= o.customer_id)
+JOIN Customer c ON c.id= o.customer_id
+GROUP BY p.id, p.name, c.segment) pq) pr 
 WHERE s_rank <= 3
 ORDER BY segment, s_rank
 
 -- 4. What are the 3 best-seller products in each product segment? (Quantity-wise)
 SELECT *
 FROM(
-SELECT qt.id, qt.name, p.segment, qt.total_quantity,
-RANK () OVER (PARTITION BY segment ORDER BY total_quantity DESC) AS q_rank
+SELECT *, 
+RANK () OVER (PARTITION BY pq.segment ORDER BY pq.total_quantity DESC) AS q_rank
 FROM(
-SELECT p.id, p.name,
+SELECT p.id, p.name, c.segment,
 SUM(oi.quantity) as total_quantity
 FROM Product p
 JOIN OrderItems oi ON p.id = oi.product_id
-GROUP BY p.id, p.name) as qt
-JOIN Product p ON p.id = qt.id)
+JOIN Orders o ON o.id = oi.order_id
+JOIN Customer c ON c.id= o.customer_id
+GROUP BY p.id, p.name, c.segment) pq) pr 
 WHERE q_rank <= 3
+ORDER BY segment, q_rank
 
 -- 5. What are the top 3 worst-selling products in every category? (Quantity-wise)
 
