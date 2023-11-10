31bcfffb3465ba82e6633ea98a6b8a4265a7eddb
Milan Karahiaur <milankrahiaur@gmail.com>
2023-11-10 21:44:52 +0100

    Update

--- Data-Pipelines/streamlit_app/streamlit_app.py
+++ Data-Pipelines/streamlit_app/streamlit_app.py
@@ -10,15 +10,18 @@ import os
 
 def get_data():
     conn = st.connection("gsheets", type=GSheetsConnection)
-    print(conn.read())
-    gc = gspread.service_account(conn.read())
-    sh = gc.open('DataPipelines')
-    worksheet = sh.worksheet('Sheet1')
-    df = pd.DataFrame(columns=['Date', 'Close'])
-    df['Date'] = worksheet.col_values(1)[1:]
-    df['Close'] = worksheet.col_values(2)[1:]
-    df['Date'] = pd.to_datetime(df['Date'])
-    return df
+    # print(conn.read())
+    # gc = gspread.service_account(conn.read())
+    # sh = gc.open('DataPipelines')
+    # worksheet = sh.worksheet('Sheet1')
+    # df = pd.DataFrame(columns=['Date', 'Close'])
+    # df['Date'] = worksheet.col_values(1)[1:]
+    # df['Close'] = worksheet.col_values(2)[1:]
+    # df['Date'] = pd.to_datetime(df['Date'])
+    url = "https://docs.google.com/spreadsheets/d/11q16a6xO9Q6u22osRzyQqEIr4ha5bJwNaCktScBlj1A/edit#gid=0"
+    data = conn.read(spreadsheet=url, usecols=[0, 1])
+    
+    return st.dataframe(data)
 
 print(get_data())
 
